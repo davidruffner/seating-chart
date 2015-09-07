9d55d0aab8223ca2c25284bb1a7ed22fe11e1bad
davidruffner <davidruffner@gmail.com>
2015-09-07 08:41:43 -0400

    Large cleanup with the code. Now it mostly follows pep convention

--- example/testseatingchart3.py
+++ example/testseatingchart3.py
@@ -1,5 +1,4 @@
-import seatingchart as sc 
-import random
+import seatingchart.seatingchart as sc
 import numpy as np
 import matplotlib.pyplot as plt
 import copy
@@ -12,17 +11,15 @@ guestlist.print_friendlist()
 nguests = len(guestlist.guests)
 seatsper = 5
 
-chart = sc.SeatingChart(guestlist,nguests/seatsper,seatsper)
-chart0=copy.deepcopy(chart)
+chart = sc.SeatingChart(guestlist, nguests/seatsper, seatsper)
+chart0 = copy.deepcopy(chart)
 print ''
 print "Guests to be seated and their friends"
 
-org = sc.Organizer(guestlist,chart)
-org0 = sc.Organizer(guestlist,chart0)
-#Seat everyone
+org = sc.Organizer(guestlist, chart)
+org0 = sc.Organizer(guestlist, chart0)
+# Seat everyone
 org.seatguests()
-
-
 
 print "The final seating chart"
 chart.print_seatingchart()
@@ -39,29 +36,27 @@ friendsT1 = np.zeros(nsteps)
 friendsT2 = np.zeros(nsteps)
 friendsT3 = np.zeros(nsteps)
 for i in steps:
-    friendsT1[i] = org1.metropolisstep(0.01)
-    friendsT2[i] = org2.metropolisstep(.1)
-    friendsT3[i] = org3.metropolisstep(1)
+    friendsT1[i] = org1.metropolisstep(temp=0.01)
+    friendsT2[i] = org2.metropolisstep(temp=.1)
+    friendsT3[i] = org3.metropolisstep(temp=1)
 
-#Now compare to what happens if i just seat people with friends
+# Now compare to what happens if i just seat people with friends
 org0.sc.print_chart()
 org0.seatguestsfriends()
-count=org0.friendcount()
+count = org0.friendcount()
 print "Number of friends with seating with friends method"
 print count
-print "friends T1,T2,T3"
-print [friendsT1[-1],friendsT2[-1],friendsT3[-1]]
-plt.plot(steps, friendsT1,label='T1')
-plt.plot(steps, friendsT2,label='T2')
-plt.plot(steps, friendsT3,label='T3')
-plt.legend( loc='upper left')
-plt.xlabel="step"
-plt.ylabel="friendships"
+print "friends T1, T2, T3"
+print [friendsT1[-1], friendsT2[-1], friendsT3[-1]]
+plt.plot(steps, friendsT1, label='T1')
+plt.plot(steps, friendsT2, label='T2')
+plt.plot(steps, friendsT3, label='T3')
+plt.legend(loc='upper left')
+plt.xlabel = "step"
+plt.ylabel = "friendships"
 
 org0.sc.to_excel('SeatingChart0.xlsx')
 org1.sc.to_excel('SeatingChart1.xlsx')
 
 
 plt.show()
-
-
--- seatingchart.py
+++ seatingchart.py
@@ -1,4 +1,7 @@
-#/usr/bin/env python
+import random
+import numpy as np
+import pandas
+
 """
 This simple program will try to sort out a seating chart from a list of
 people and their inter-relationships.
@@ -6,35 +9,28 @@ people and their inter-relationships.
 Modifications:
  3/9/2014  written by David Ruffner
 """
-import random
-import numpy as np
-import pandas
-#Make classes 
 
-class Guest():
+
+class Guest(object):
     def __init__(self, name, friends=None):
         """
         Constructs a guest object
 
         Inputs
         -----
-        :param name:
-        :param friends:
-
-        Outputs
-        ------
-        :return:
+        :param name: string
+        :keyword friends: list of strings
         """
         self.name = name
         if friends is None:
             self.friendnames = list()
         else:
             self.set_friendnames(friends)
-        
-    def set_name(self,newname):
+
+    def set_name(self, newname):
         self.name = newname
-        
-    def set_friendnames(self,friendnames):
+
+    def set_friendnames(self, friendnames):
         self.friendnames = friendnames
 
     def get_name(self):
@@ -43,42 +39,55 @@ class Guest():
     def get_friendnames(self):
         return self.friendnames
 
-class GuestList():
-    def __init__(self,guestnames=None):
+
+class GuestList(object):
+    def __init__(self, guestnames=None):
         """
-        Guest list constructor
-        :param guestnames: list
-        :return:
+        Makes a list of Guest objects and of guest names,
+        and a dictionary relating the names to the objects
+
+        Inputs
+        -------
+        :keyword guestnames: list of strings
         """
         if guestnames is None:
             self.guests = list()
         else:
             self.guests = [Guest(name) for name in guestnames]
-        self.guestdict = {g.get_name():g for g in self.guests}
+        self.guestdict = {g.get_name(): g for g in self.guests}
 
     def fromExcel(self, excelfile):
+        """
+        Imports a guest list from an excel file.
+
+        The file should have the guest names on the leftmost column and then
+        friend names in adjacent columns
+
+        Uses python pandas for the import. It also won't repeat names.
+
+        :param excelfile:
+        """
         peopledf = pandas.read_excel(excelfile)
-        #Read in all the names
+        # Read in all the names
         allnames = set()
         for column in peopledf[1:]:
             for rownum in peopledf.index:
                 name = peopledf.iloc[rownum][column]
-                #print rownum, column, name
+                # print rownum, column, name
                 if name is np.nan:
                     continue
                 allnames.add(name)
         self.guests = [Guest(name) for name in allnames]
-        self.guestdict = {g.get_name():g for g in self.guests}
+        self.guestdict = {g.get_name(): g for g in self.guests}
 
-        #Find all the friends
+        # Find all the friends
         for rownum in peopledf.index:
             friends = [str(name) for name in peopledf.iloc[rownum].values if name is not np.nan]
             [self.guestdict[friend].set_friendnames(friends) for friend in friends]
 
-
-    def insert_guest(self,guest):
+    def insert_guest(self, guest):
         self.guests.append(guest)
-        self.guestdict[guest.get_name()]=guest
+        self.guestdict[guest.get_name()] = guest
 
     def print_list(self):
         for guest in self.guests:
@@ -89,52 +98,58 @@ class GuestList():
         for guest in self.guests:
             name = guest.get_name()
             friends = guest.get_friendnames()
-            print name+":"+",".join(friends)
+            print name + ":" + ",".join(friends)
 
-class SeatingChart():
-    def __init__(self,guestlist,tables,seatper):
+
+class SeatingChart(object):
+    def __init__(self, guestlist, tables, seatper):
+        """
+        Object for keeping track of where guests will be sitting
+        :param guestlist: list of guest objects
+        :param tables: int number of tables
+        :param seatper: int number of people per table
+        """
         self.chart = [['' for x in xrange(seatper)] for y in xrange(tables)]
         self.seatdict = dict()
-        self.open = [(y,x) for x in xrange(seatper) for y in xrange(tables)]
+        self.open = [(y, x) for x in xrange(seatper) for y in xrange(tables)]
         self.seated = list()
         self.toseat = [g.get_name() for g in guestlist.guests]
-        self.gl= guestlist
+        self.gl = guestlist
 
     def pick_rand(self):
         if len(self.toseat) == 0:
             return None
         randguestname = random.choice(self.toseat)
-        return randguestname    
+        return randguestname
 
-    def pick_friend_of(self,guestname):
+    def pick_friend_of(self, guestname):
         guest = self.gl.guestdict[guestname]
         friendnames = list(guest.get_friendnames())
         if len(friendnames) == 0:
-            return None #It returns none if there are no friends of this
-                        #schmuck who need to be seated
+            return None  # It returns none if there are no friends of this
+            # schmuck who need to be seated
         random.shuffle(friendnames)
         for f in friendnames:
             if self.toseat.count(f) == 0:
                 pass
             else:
                 return f
-        print "The schmuck has no friends or they are already seated"        
+        print "The schmuck has no friends or they are already seated"
         return None
 
-
-    def seat_guest(self,guestname):
-        n=guestname
+    def seat_guest(self, guestname):
+        n = guestname
         seatindex = self.open.pop(random.randrange(len(self.open)))
         self.chart[seatindex[0]][seatindex[1]] = n
 
-        #update the seatdict, and seated
+        # update the seatdict, and seated
         self.seated.append(n)
-        self.seatdict[n]=seatindex
+        self.seatdict[n] = seatindex
         self.toseat.remove(n)
 
-    def seat_guest_here(self,guestname,index):
-        n=guestname
-        #check if the seat is open
+    def seat_guest_here(self, guestname, index):
+        n = guestname
+        # check if the seat is open
         if self.open.count(index) == 0:
             print "the seat is already filled!!"
             return None
@@ -142,27 +157,35 @@ class SeatingChart():
             self.open.remove(index)
         self.chart[index[0]][index[1]] = n
 
-        #update the seatdict, and seated
+        # update the seatdict, and seated
         self.seated.append(n)
-        self.seatdict[n]=index
+        self.seatdict[n] = index
         self.toseat.remove(n)
 
-    def seat_guestfriend(self,guestname,friendname):
-        #Seats the friend of a guest at the guests table
-       
-        #First need to find the table the guest is sitting at 
-        # and then the open seats at that table
+    def seat_guestfriend(self, guestname, friendname):
+        """
+        Seats the friend of a guest at the guests table
+
+        First need to find the table the guest is sitting at
+        and then the open seats at that table
+
+        Inputs
+        -------
+        :param guestname: string
+        :param friendname: string
+        :return:
+        """
         index = self.seatdict[guestname]
         tablenum = index[0]
-        openseats = [(x,y) for x,y in self.open if x==tablenum]
-        if not(len(openseats)==0):
+        openseats = [(x, y) for x, y in self.open if x == tablenum]
+        if not (len(openseats) == 0):
             seatindex = openseats.pop(random.randrange(len(openseats)))
             self.chart[seatindex[0]][seatindex[1]] = friendname
 
-            #update the seatdict, and seated
+            # update the seatdict, and seated
             self.open.remove(seatindex)
             self.seated.append(friendname)
-            self.seatdict[friendname]=seatindex
+            self.seatdict[friendname] = seatindex
             self.toseat.remove(friendname)
             return seatindex
         else:
@@ -173,24 +196,23 @@ class SeatingChart():
             return None
         randguestname = random.choice(self.seated)
         index = self.seatdict[randguestname]
-        #update lists and dictionaries
-        
+        # update lists and dictionaries
+
         self.chart[index[0]][index[1]] = ''
-        self.seatdict.pop(randguestname,None)
+        self.seatdict.pop(randguestname, None)
         self.open.append(index)
         self.seated.remove(randguestname)
         self.toseat.append(randguestname)
-        return [randguestname,index]    
+        return [randguestname, index]
 
-    def remove_guest_here(self,guestname,index):
-        n=guestname
+    def remove_guest_here(self, guestname, index):
+        n = guestname
         self.chart[index[0]][index[1]] = ''
-        self.seatdict.pop(n,None)
+        self.seatdict.pop(n, None)
         self.open.append(index)
         self.seated.remove(n)
         self.toseat.append(n)
-        return [n,index]    
-
+        return [n, index]
 
     def print_chart(self):
         print self.chart
@@ -198,34 +220,40 @@ class SeatingChart():
     def print_seatingchart(self):
         for n in self.seated:
             index = self.seatdict[n]
-            print (n,index)
+            print (n, index)
 
     def to_excel(self, outfile='SeatingChart.xlsx'):
         chartdf = pandas.DataFrame(self.chart)
         chartdf.index = ["Table {0}".format(i) for i in chartdf.index]
         chartdf.to_excel(outfile)
 
-        
-class Organizer():
-    def __init__(self,guestlist,seatingchart):
+
+class Organizer(object):
+    def __init__(self, guestlist, seatingchart):
         self.gl = guestlist
         self.sc = seatingchart
         self.friendships = 0
 
     def seatguests(self):
-        #This method seats all of the guests randomly
+        """
+        This method seats all of the guests randomly
+        :return:
+        """
         print "Seating guests and their friends"
         nguests = len(self.gl.guests)
         for i in xrange(nguests):
             randguest = self.sc.pick_rand()
             if randguest is None:
                 print "exit for loop"
-                break    
+                break
             else:
                 self.sc.seat_guest(randguest)
 
     def seatguestsfriends(self):
-        #This method seats all of the guests and tries to put the friends
+        """
+        This method seats all of the guests and tries to put the friends
+        :return:
+        """
         # at the same table
         print "Seating guests and their friends"
         nguests = len(self.gl.guests)
@@ -238,92 +266,92 @@ class Organizer():
             self.sc.seat_guest(rgn)
             randguest = self.gl.guestdict[rgn]
             friends = randguest.get_friendnames()
-            print "Seated " + rgn+",seating friends also..."
+            print "Seated " + rgn + ", seating friends also..."
             for f in friends:
-                friend=self.sc.pick_friend_of(randguestname)
-                if not(friend is None):
-                    seatindex = self.sc.seat_guestfriend(rgn,friend)
+                friend = self.sc.pick_friend_of(randguestname)
+                if not (friend is None):
+                    seatindex = self.sc.seat_guestfriend(rgn, friend)
                     if seatindex is None:
                         self.sc.seat_guest(friend)
                         print "There is no room at the table for the friend"
-    
-    def tablefriendcount(self,tablenum):
+
+    def tablefriendcount(self, tablenum):
         count = 0
         names = self.sc.chart[tablenum]
-        for i,n in enumerate(names):
+        for i, n in enumerate(names):
             try:
                 gobj = self.gl.guestdict[n]
-            except:
+            except KeyError:
                 continue
             fns = gobj.get_friendnames()
-            #Find the friends that are at the table by set intersection
+            # Find the friends that are at the table by set intersection
             friendshere = list(set(fns).intersection(set(names)))
-            count+=len(friendshere)/2.
+            count += len(friendshere) / 2.
         return count
 
     def friendcount(self):
         count = 0
         ntables = len(self.sc.chart)
         for i in xrange(ntables):
-            count+= self.tablefriendcount(i)
+            count += self.tablefriendcount(i)
         return count
 
     def switch_rand(self):
-        #Remove the two guests, saving their info and checking if possible
+        # Remove the two guests, saving their info and checking if possible
         info1 = self.sc.pick_randremove()
         if info1 is None:
             return None
         info2 = self.sc.pick_randremove()
         if info2 is None:
             return None
-        #Switch their seats
-        self.sc.seat_guest_here(info1[0],info2[1])
-        self.sc.seat_guest_here(info2[0],info1[1])
+        # Switch their seats
+        self.sc.seat_guest_here(info1[0], info2[1])
+        self.sc.seat_guest_here(info2[0], info1[1])
 
-    def metropolisstep(self,temp):
-        #This method switches two friends and checks to see if it makes 
-        #things better. If it doesn't it still makes the switch with some
-        #probability that is defined by the effective temperature in 
-        # units of friendships
+    def metropolisstep(self, temp=0.1):
+        """
+        This method switches two friends and checks to see if it makes
+        things better. If it doesn't it still makes the switch with some
+        probability that is defined by the effective temperature in
+        units of friendships
 
-        #First check the number of friends to get a baseline
+        Inputs
+        ------
+        :keyword temp: int, specifies how much random swiching will be done
+        :return:
+        """
+
+        # First check the number of friends to get a baseline
         friendcount0 = self.friendcount()
-        #Remove the two guests, saving their info and checking if possible
+        # Remove the two guests, saving their info and checking if possible
         info1 = self.sc.pick_randremove()
         if info1 is None:
             return None
         info2 = self.sc.pick_randremove()
         if info2 is None:
             return None
-        #Switch their seats
-        self.sc.seat_guest_here(info1[0],info2[1])
-        self.sc.seat_guest_here(info2[0],info1[1])
-        #First check the number of friends again to see if it improved
+        # Switch their seats
+        self.sc.seat_guest_here(info1[0], info2[1])
+        self.sc.seat_guest_here(info2[0], info1[1])
+        # First check the number of friends again to see if it improved
         friendcount1 = self.friendcount()
-        
+
         if friendcount1 >= friendcount0:
-            #If there are more friends than keep the switch!
+            # If there are more friends than keep the switch!
             print "Made the switch"
             return friendcount1
         else:
-            delfriends = friendcount1-friendcount0
-            expdelfriends = np.exp(delfriends/temp)
+            delfriends = friendcount1 - friendcount0
+            expdelfriends = np.exp(delfriends / temp)
             rand = random.random()
             if rand >= expdelfriends:
-                #if there are fewer friends then put it back
+                # if there are fewer friends then put it back
                 print "better to keep the old way"
-                self.sc.remove_guest_here(info1[0],info2[1])
-                self.sc.remove_guest_here(info2[0],info1[1])
-                self.sc.seat_guest_here(info1[0],info1[1])
-                self.sc.seat_guest_here(info2[0],info2[1])
+                self.sc.remove_guest_here(info1[0], info2[1])
+                self.sc.remove_guest_here(info2[0], info1[1])
+                self.sc.seat_guest_here(info1[0], info1[1])
+                self.sc.seat_guest_here(info2[0], info2[1])
                 return friendcount0
             else:
                 print "it's temporarily worse but keep it"
                 return friendcount1
-            
-
-
-
-
-
-
